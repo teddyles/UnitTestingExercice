37a66fdc3da9d2d5a18079771681fba34784778b
unknown <teddy-lesueur@orange.fr>
2020-01-17 12:58:27 +0100

    Initial commit

---
+++ src/test/java/exercices/TestRectangle.java
@@ -0,0 +1,8 @@
+package exercices;
+
+public class TestRectangle {
+    public static void main (String[] args){
+        Rectangle monRectangle = new Rectangle(4.0,5.0);
+        monRectangle.afficher();
+    }
+}
